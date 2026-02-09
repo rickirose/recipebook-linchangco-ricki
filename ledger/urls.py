d67ca55325ad3f426439d7eb95cfadef5edce3b5
Ricki Linchangco <rickirose.business@gmail.com>
2026-02-09 21:25:28 +0800

    Changed root url to recipe list

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import ledger, recipe_list, recipe_1, recipe_2
 
 urlpatterns = [
-    path('', ledger, name='ledger'),
-    path('recipes/list/', recipe_list, name='recipe-list'),
-    path('recipes/recipe/1/', recipe_1, name='recipe-1'),
-    path('recipes/recipe/2/', recipe_2, name='recipe-2'),
+    path('', recipe_list, name='recipe-list'),
+    path('recipe/1/', recipe_1, name='recipe-1'),
+    path('recipe/2/', recipe_2, name='recipe-2'),
 ]
