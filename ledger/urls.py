0a58e89860be57e343d88765c85a66ea062e4cc7
rickirose <rickirose.business@gmail.com>
2026-03-12 16:20:01 +0800

    Add redirect and password_reset_done file

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RecipesListView, RecipeView
 
 urlpatterns = [
-    path('recipes/list', RecipesListView.as_view(), name="recipes_list"),
+    path('recipes/list', RecipesListView.as_view(), name="recipes-list"),
     path('<int:pk>/', RecipeView.as_view(), name="recipe"),
 ]
--- recipebook/settings.py
+++ recipebook/settings.py
@@ -117,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Redirect
+LOGIN_REDIRECT_URL = 'recipes-list'
+LOGOUT_REDIRECT_URL = 'login'
